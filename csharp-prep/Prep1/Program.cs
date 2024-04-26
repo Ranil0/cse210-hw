7806e458bfba99f06ff620a39cee60250ef26aba
Ranil0 <pol23012@byui.edu>
2024-04-26 16:39:25 +0400

    added comment

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
+        // Prep1
         Console.Write("What is your first name? ");
         string first = Console.ReadLine();
         
