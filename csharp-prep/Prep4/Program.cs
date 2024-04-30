e4bc009b3ab8428df4095f3a02b15312cf5c939c
Ranil0 <pol23012@byui.edu>
2024-04-30 15:26:29 +0400

    updated file

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -5,9 +5,10 @@ class Program
 {
     static void Main(string[] args)
     {
-    //prep4
-    List<int> numbers = new List<int>();
-    int userNumber = -1;
+        List<int> numbers = new List<int>();
+        
+       
+        int userNumber = -1;
         while (userNumber != 0)
         {
             Console.Write("Enter a number (0 to quit): ");
@@ -15,14 +16,13 @@ class Program
             string userResponse = Console.ReadLine();
             userNumber = int.Parse(userResponse);
             
-            
             if (userNumber != 0)
             {
                 numbers.Add(userNumber);
             }
         }
 
-        
+       
         int sum = 0;
         foreach (int number in numbers)
         {
@@ -31,9 +31,11 @@ class Program
 
         Console.WriteLine($"The sum is: {sum}");
 
+       
         float average = ((float)sum) / numbers.Count;
         Console.WriteLine($"The average is: {average}");
 
+        
         int max = numbers[0];
 
         foreach (int number in numbers)
