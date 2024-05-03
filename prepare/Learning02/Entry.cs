ddadc79bb89aecaacedfea77772384c39182e22d
Ranil0 <pol23012@byui.edu>
2024-05-03 17:50:03 +0400

    added file to Entry.cs

--- prepare/Learning02/Entry.cs
+++ prepare/Learning02/Entry.cs
@@ -1,14 +1,37 @@
 using System;
 
-public class Job
+public class Entry 
 {
-    public string _jobTitle;
-    public string _company;
-    public int _startYear;
-    public int _endYear;
+    public string _date;
+    public string _prompt;
+    public string _response;
 
-    public void Display()
+    public void GenerateDate()
     {
-        Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
+        DateTime theCurrentTime = DateTime.Now;
+        _date = theCurrentTime.ToShortDateString();
+    }
+
+    public void GeneratePrompt()
+    {
+        List<string> prompts = new List<string>()
+        {
+        "What new skill or knowledge did you acquire today, if any?",
+        "Did you encounter any unexpected challenges today, and how did you handle them?",
+        "How did I see the hand of the Lord in my life today?",
+        "How did you prioritize your tasks or activities today, and did it align with your goals or values?",
+        "Did you engage in any acts of kindness or generosity today, and how did it make you feel?",
+        };
+
+        Random rand = new Random();
+        int randomIndex = rand.Next(prompts.Count);
+        _prompt = prompts[randomIndex];
+        Console.WriteLine(_prompt);
+    }
+
+    public void GetResponse()
+    {
+        Console.Write("> ");
+        _response = Console.ReadLine();
     }
 }
