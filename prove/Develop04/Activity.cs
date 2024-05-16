8ce13af05262ab43884a7c480da090d7e2649196
Ranil0 <pol23012@byui.edu>
2024-05-16 11:31:19 +0400

    added class Activity and files

---
+++ prove/Develop04/Activity.cs
@@ -0,0 +1,26 @@
+public class Activity {
+
+  private string _name;
+  private string _description;
+  private int _duration;
+  
+  public Activity() {
+
+  }
+
+  public void StartingMessage() {
+
+  }
+
+  public void EndingMessage() {
+
+  }
+
+  public void ShowSpinner(int seconds) {
+
+  }
+
+  public void CountDown(int seconds) {
+
+  }
+}
